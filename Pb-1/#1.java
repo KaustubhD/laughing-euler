dda131cbb5328870fd7d521c1656a5193e91ec7b
Kaustubh <zzKDzz@users.noreply.github.com>
2019-01-25 22:36:00 +0530

    Update #1.java

--- Pb-1/#1.java
+++ Pb-1/#1.java
@@ -1,23 +1,21 @@
 //#1
 
-  public class HelloWorld{
-    public static void main(String []args){
-        
-      System.out.println(summ(19564));
-         
-         
-    }
-    private static long summ(long n){
-      long num =0;
-        
-        for(long i=3;i < n;i++){
-          if(i % 3 == 0 || i % 5 == 0){
-            num+=i;
-                
+class HelloWorld{
+  public static void main(String []args){
+
+    System.out.println(summ(19564));  
+
+  }
+  public static long summ(long n){
+    long num=0;
+
+      for(long i=3;i<n;i++){
+        if(i % 3 == 0 || i % 5 == 0){
+          num+=i;
+
         }
-            
-        }
-           return num;
-        
-    }
-  } 
+
+      }
+      return num;
+  }
+} 
