91f9126aaaaa2a8bc830323701c01b5be0f63b13
svikash <svikas265@gmail.com>
2019-01-26 19:40:18 +0530

    #2.java
    second commit

---
+++ Pb-2/#2.java
@@ -0,0 +1,25 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+    
+     System.out.println(fiboEvenSum(23));  
+     }
+    public static int fiboEvenSum(int n){
+    int a=0;
+    int b=1;
+    int c=0;
+    int sum=0;
+    while( c < n){
+      c=a+b;
+      a=b;
+      b=c;
+      if(c % 2 == 0 && c < n){
+             
+          sum+=c;
+        }
+           
+           
+    }
+      return sum;
+  }
+}
