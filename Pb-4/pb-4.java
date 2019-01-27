ea172b1438a0654e9befac861f2724312e7e5e8c
svikash <svikas265@gmail.com>
2019-01-27 17:38:04 +0530

    Create pb-4.java

---
+++ Pb-4/pb-4.java
@@ -0,0 +1,33 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+   System.out.println(largestPrimeFactor(2));  
+  }
+
+  public static double largestPrimeFactor(double n){
+
+    double reverse=0;
+    
+    double b= (Math.pow(10,n)-1);
+    for(int i=1;i<=b;i++){
+        
+        //System.out.println(i);
+        for(int j=1;j<=i;j++){
+            n =i*j;
+            
+            String c=String.valueOf(n);
+             StringBuilder c1 = new StringBuilder(); 
+             c1.append(c); 
+             c1 = c1.reverse();
+             System.out.println(c);
+             
+        
+            
+        }
+        
+    }     
+        return n; 
+  }
+}
+    
