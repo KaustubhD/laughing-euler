0a18a865f6db004540fd58a86d9b16ea47ffd913
svikash <svikas265@gmail.com>
2019-01-28 22:31:58 +0530

    pb-5.java
    solution

--- Pb-4/pb-4.java
+++ Pb-4/pb-4.java
@@ -1,34 +1,41 @@
 public class HelloWorld{
 
-  public static void main(String []args){
+     public static void main(String []args){
         
-    System.out.println(largestPalindromeProduct(2));  
+   System.out.println(largestPalindromeProduct(2));  
   }
 
   public static double largestPalindromeProduct(int n){
 
-    double max=0;
+  double max=0;
     
     double b= (Math.pow(10,n)-1);
     for(int i=(int)b;i>=1;i--){
         
-      for(int j=i;j>=1;j--){
-        n =i*j;
-	if(n>max){
-
-          String c=String.valueOf(n);
-          StringBuilder c1 = new StringBuilder(); 
-          c1.append(c); 
-          c1 = c1.reverse();
-          if(c.equals(c1.toString())){
-
-	    max=n;
-
-          }
-	}      
-      }
+        //System.out.println(i);
+        for(int j=i;j>=1;j--){
+             n =i*j;
+			 if(n>max){
+				 
+			 
+            //System.out.println(n);
+             String c=String.valueOf(n);
+             StringBuilder c1 = new StringBuilder(); 
+            c1.append(c); 
+             c1 = c1.reverse();
+             if(c.equals(c1.toString())){
+                 
+                 max=n;
+                 
+             }
+			 }
+             
+        
+            
+        }
+        
     }     
-    return max; 
+        return max; 
   }
 }
     
