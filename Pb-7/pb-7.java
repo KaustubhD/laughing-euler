08c12734a29d5896b53beae4df985d181484dbb8
Kaustubh <zzKDzz@users.noreply.github.com>
2019-01-31 11:44:05 +0530

    Update styles

--- Pb-7/pb-7.java
+++ Pb-7/pb-7.java
@@ -11,18 +11,18 @@ public class HelloWorld{
     int i=0; int x = 1;
     while(i < n){
       x++;
-      if(abc(x)){
+      if(isPrime(x)){
         i++;
       }
     }
-        return x;
+    return x;
   } 
-  public static boolean abc(long n) {
+  public static boolean isPrime(int n) {
     if (n < 2) return false;
-      else if (n == 2) return true;
-        for (int i = 2; i < n/2 + 1; i++)
-          if (n % i == 0)
-            return false;
-        return true;
-    }
+    else if (n == 2) return true;
+    for (int i = 2; i < n/2 + 1; i++)
+      if (n % i == 0)
+        return false;
+    return true;
+  }
 }
