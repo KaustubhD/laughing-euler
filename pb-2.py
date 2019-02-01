2c5e12642f09903ccf167ee80276f9596bfc1af1
Kaustubh <zzKDzz@users.noreply.github.com>
2019-02-01 13:06:00 +0530

    Update styles

--- pb-2.py
+++ pb-2.py
@@ -1,14 +1,15 @@
+#pb-2
 def fiboEvenSum(n):
   n1=0
   n2=1
   count=0
   sum=0
   while count<n:
-      count=n1+n2
-      n1=n2
-      n2=count
-      if count%2==0:
-        sum+=count
+    count=n1+n2
+    n1=n2
+    n2=count
+    if count%2==0:
+      sum+=count
   return sum
 
 if __name__== "__main__":
