de33aec1e6b11216c816fe2756656914263c0ab2
svikash <svikas265@gmail.com>
2019-02-02 20:44:32 +0530

    Create pb-10.java

---
+++ Pb-10/pb-10.java
@@ -0,0 +1,24 @@
+//#pb-10
+public class HelloWorld{
+  public static void main(String []args){
+    System.out.println(primeSummation(140759));
+  }
+   public static int primeSummation(int n){
+    int sum=0;
+    for (int i = 2; i < n; i++) {
+      int count = 0;
+      for (int j = 2; j <= i / 2; j++) {
+        if (i % j == 0) {
+          count++;
+          break;
+        }
+      }
+      if(count ==0){
+  //System.out.println(i);
+        sum+=i;
+      }
+    }   
+        return sum;
+  }
+    
+}
