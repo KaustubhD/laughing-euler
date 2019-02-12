1582473d30221793325b5d264f889d5f80474962
svikash <svikas265@gmail.com>
2019-02-12 12:52:22 +0530

    solution pb-12

---
+++ Pb-12/pb-12.java
@@ -0,0 +1,53 @@
+public class HelloWorld{
+  public static void main(String []args){
+    long startTime = System.nanoTime();
+    System.out.println(divisibleTriangleNumber(23));
+    long endTime = System.nanoTime();
+    long timeElapsed = endTime - startTime;
+    System.out.println("Execution time in microseconds : " + timeElapsed / 1000);
+  }
+  public static int divisibleTriangleNumber(int n ){
+    int i=1;
+    int a=0;
+    while(true){
+      a=i*(i+1) /2 ;
+      if(factor(a)>=n){
+        break;
+            
+      }    
+        i++;
+    }
+        return a;
+  }
+  public static int factor(int n){
+    int count =0;
+    for(int j=1;j<=n;j++){
+      if(n % j ==0){
+        count++;
+      }
+    }    
+        return count;
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
