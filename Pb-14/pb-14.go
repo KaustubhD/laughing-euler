795eb34bffe9e00c311eb76d932fe7468ca1af84
zzKDzz <kaustubhd287@gmail.com>
2019-02-16 21:30:24 +0530

    Still trying

--- Pb-14/pb-14.go
+++ Pb-14/pb-14.go
@@ -7,17 +7,20 @@ import(
 )
 
 func longestCollatzSequence(n int) int{
-  var coll_lens = make([]int, n) // Array of collatz lengths of all numbers till 'n'
+  var coll_lens = make([]int, n + 1) // Array of collatz lengths of all numbers till 'n'
   max := [2]int{0, 0}
 
   coll_lens[1] = 1
 
-  for i := 0; i < n; i++{ // checking every number till 'n'
+  for i := 1; i <= n; i++{ // checking every number till 'n'
     var ith, count int = i, 0
     intermediates := make([]int, 0)
-    for ith > n - 1 || count < 3 || coll_lens[ith] < 1{
-      intermediates = append(intermediates, ith)
-      fmt.Println(intermediates)
+    for ith > n - 1 || coll_lens[ith] >= 1{
+      if ith < n{
+        intermediates = append(intermediates, ith)
+      }
+      fmt.Println(ith, n - 1)
+      // fmt.Println(intermediates)
       if ith % 2 == 0{
         ith /= 2
       }else{
@@ -26,14 +29,15 @@ func longestCollatzSequence(n int) int{
       count += 1
     }
 
-    for ind, intermed := range intermediates{
-      intermed_coll_length := coll_lens[count] + len(intermediates) - ind
+    for _, intermed := range intermediates{
+      fmt.Println("intermed : ", intermed, " and its len", coll_lens[intermed])
+      intermed_coll_length := coll_lens[count] + len(intermediates)
       coll_lens[intermed] = intermed_coll_length
       if intermed_coll_length > max[1]{
         max[0], max[1] = intermed, intermed_coll_length
       }
     }
-
+    fmt.Println("Length ", i,":", coll_lens[i])
   }
   return max[0]
 }
@@ -46,3 +50,5 @@ func main(){
   elapsed := time.Since(start)
   fmt.Printf("Time taken: %s\n", elapsed)
 }
+
+// 7 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
