6ff1a783e3551184239960ed165a9bd57cf82e24
zzKDzz <kaustubhd287@gmail.com>
2019-03-08 22:38:30 +0530

    Solution of 10 in go

--- Pb-10/pb-10.go
+++ Pb-10/pb-10.go
@@ -3,53 +3,29 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"time"
 )
 
-func getSum(p, a float64) int {
-	fmt.Println("Factor", p, "is", a, "times")
-	dobP := (math.Pow(p, a+1) - 1) / (p - 1)
-	return int(dobP)
-}
-
 func primeSummation(n int) int {
-	var i, count, sum int = 3, 0, 0
-	for n%2 == 0 {
-		n /= 2
-		count++
-	}
-	sum += getSum(float64(2), float64(count))
-	for n != 1 {
-		count = 0
-		for n%i == 0 {
-			n /= i
+	var MAX = n
+	var sum int = 0
+	var arr = make([]bool, n)
+	var count int = 0
+	arr[0], arr[1] = true, true
+	for i := 0; i < MAX; i++ {
+		//fmt.Println(i)
+		if !arr[i] {
+			sum += i
+			for j, k := i*2, 0; k < ((MAX-1)/i)-1; k++ {
+				arr[j] = true
+				j += i
+			}
 			count++
 		}
-		if count > 0 {
-			sum += getSum(float64(i), float64(count))
+		if count == n {
+			return sum
 		}
-		i += 2
 	}
-	// var MAX = n
-	// var sum int = 0
-	// var arr  = make([]bool, n)
-	// var count int = 0
-	// arr[0], arr[1] = true, true
-	// for i := 0; i < MAX; i++{
-	//   //fmt.Println(i)
-	//   if !arr[i]{
-	//     sum += i
-	//     for j, k := i * 2, 0; k < ((MAX - 1)/i) - 1; k++{
-	//       arr[j] = true
-	//       j += i
-	//     }
-	//     count += 1
-	//   }
-	//    if count == n{
-	//      return sum
-	//    }
-	// }
 	return sum
 }
 
